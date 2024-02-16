f7fe54feac0b1974101ade168ee5605ad1f95c43
Biresh <js.biresh@gmail.com>
2024-02-16 18:22:26 +0530

    changed the winning score to 110, which will be changed later on

--- script.js
+++ script.js
@@ -103,7 +103,7 @@ btnHold.addEventListener('click', function () {
       scores[activePlayer];
 
     // 2. Check if score >= 100.
-    if (scores[activePlayer] >= 100) {
+    if (scores[activePlayer] >= 110) {
       // Finish the game
       playing = false;
       diceEl.classList.add('hidden');
